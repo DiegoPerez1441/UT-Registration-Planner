3329f83cffb379990c6c8faf811eb5fc413dd039
DiegoPerez1441 <devdiegoperez@gmail.com>
2022-05-24 20:56:44 -0500

    Add example CourseCard.tsx

---
+++ ut-registration-planner/components/CourseCard.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Paper from "@mui/material/Paper"
+
+type Props = {}
+
+const CourseCard = (props: Props) => {
+    return (
+        <Paper elevation={1}>
+            UT 101 - Example Professor
+        </Paper>
+    )
+}
+
+export default CourseCard
