80b53649eb267dd3b9736c7065c53df80ab3e1b1
DiegoPerez1441 <devdiegoperez@gmail.com>
2022-05-24 21:55:17 -0500

    Add example CourseCard styling

--- ut-registration-planner/components/CourseCard.tsx
+++ ut-registration-planner/components/CourseCard.tsx
@@ -1,12 +1,16 @@
 import React from "react"
 import Paper from "@mui/material/Paper"
+import Typography from "@mui/material/Typography"
+
+import styles from "./CourseCard.module.scss"
 
 type Props = {}
 
 const CourseCard = (props: Props) => {
     return (
-        <Paper elevation={1}>
-            UT 101 - Example Professor
+        <Paper elevation={4} className={styles.container}>
+            <h2 className={styles.h2}>UT 101 - Example Professor</h2>
+            <p className={styles.p}>10234</p>
         </Paper>
     )
 }
