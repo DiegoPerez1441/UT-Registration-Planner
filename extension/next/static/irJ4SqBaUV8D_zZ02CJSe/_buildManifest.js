7409ce90f7e72386df425d61c973ca932f8e0ee6
DiegoPerez1441 <devdiegoperez@gmail.com>
2022-05-27 22:32:15 -0500

    Add npm run build output

---
+++ extension/next/static/irJ4SqBaUV8D_zZ02CJSe/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/759-6320311098957cea.js","static/css/65d040c0a964104d.css","static/chunks/pages/index-d72e9865bac56548.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
