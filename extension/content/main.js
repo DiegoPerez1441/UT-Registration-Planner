f90dfcccd14801f10b13634ceb92daa3c2476c3a
DiegoPerez1441 <devdiegoperez@gmail.com>
2022-06-02 21:09:20 -0500

    Add sample chrome.storage.sync use

--- extension/content/main.js
+++ extension/content/main.js
@@ -1,1 +1,10 @@
-$("td[data-th='Unique']").children("a").css("color", "#bf5700")
+// Sample chrome.storage.sync use
+// chrome.storage.sync.set({ key: value }, function () {
+//   console.log("Value is set to " + value);
+// });
+
+// chrome.storage.sync.get(["key"], function (result) {
+//   console.log("Value currently is " + result.key);
+// });
+
+$("td[data-th='Unique']").children("a").css("color", "#bf5700");
