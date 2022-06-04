d311364e33b456c2f33d750cd718d53c3ef2d0a7
DiegoPerez1441 <devdiegoperez@gmail.com>
2022-06-03 20:20:28 -0500

    Add background script

---
+++ extension/content/background.js
@@ -0,0 +1,11 @@
+console.log("background.js is working")
+
+// Synchronous response to storage updates
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+    for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+        console.log(
+            `Storage key "${key}" in namespace "${namespace}" changed.`,
+            `Old value was "${oldValue}", new value is "${newValue}".`
+        )
+    }
+})
