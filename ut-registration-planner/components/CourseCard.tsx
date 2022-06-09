d5dff1117284f5e81f458795b92791a5e5d5b1dc
DiegoPerez1441 <devdiegoperez@gmail.com>
2022-06-08 19:45:42 -0500

    CourseCard.tsx now supports displaying multiple instructors

--- ut-registration-planner/components/CourseCard.tsx
+++ ut-registration-planner/components/CourseCard.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from 'react';
 import Paper from "@mui/material/Paper"
 import Typography from "@mui/material/Typography"
 import ContentCopyIcon from "@mui/icons-material/ContentCopy"
@@ -11,7 +11,7 @@ import { getStorage, setStorage } from "../utils/chromeStorage"
 interface Course {
     name: string,
     fullName: string,
-    instructor: string,
+    instructor: string[],
     uid: number,
     status: string,
     // time: time
@@ -26,12 +26,23 @@ const truncate = (str: string, n: number): string => {
 }
 
 const CourseCard = ({ course }: CourseCardProps) => {
+
+    const [instructorsText, setInstructorsText] = useState("")
+
+    useEffect(() => {
+        if (course.instructor.length > 1) {
+            setInstructorsText(`${course.instructor[0]} and ${course.instructor[1]}`)
+        } else if (course.instructor.length == 1) {
+            setInstructorsText(course.instructor[0])
+        }
+    }, [])
+
     return (
         <Paper elevation={4} className={styles.container}>
             <div className={styles.textContainer}>
                 {/* <h2 className={styles.h2}>UT 101 - Example Professor</h2> */}
                 <h2 className={styles.h2}>
-                    {truncate(course.name + " - " + course.instructor, 25)}
+                    {truncate(course.name + " - " + instructorsText, 25)}
                 </h2>
                 <p className={styles.p}>{course.uid}</p>
             </div>
