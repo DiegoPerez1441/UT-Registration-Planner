3266ec17d3c00ac39076ebce05cd410fc6e5bc90
DiegoPerez1441 <devdiegoperez@gmail.com>
2022-06-11 20:08:23 -0500

    Add npm run build output

---
+++ extension/next/static/5WjDNbaNVuTxJkTZNjDVK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/808-63a8549a95e8813e.js","static/css/815d621a89d5cc96.css","static/chunks/pages/index-81ffc97e2f3a1caf.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- extension/next/static/iFcviE-Q4nrV_xyGg6gD9/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/808-63a8549a95e8813e.js","static/css/815d621a89d5cc96.css","static/chunks/pages/index-4c15e1795da45977.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
